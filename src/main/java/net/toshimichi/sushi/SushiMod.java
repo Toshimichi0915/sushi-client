43031243fec17c3781117317d41d505ec2ed2711
Toshimichi0915 <main@toshimichi.net>
2021-03-06 03:57:14 +0900

    Initialize repository

---
+++ src/main/java/net/toshimichi/sushi/SushiMod.java
@@ -0,0 +1,23 @@
+package net.toshimichi.sushi;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "sushi", name = "Sushi Client", version = "1.0")
+public class SushiMod {
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
---
+++ src/main/java/net/toshimichi/sushi/mixin/MixinLoaderForge.java
@@ -0,0 +1,41 @@
+package net.toshimichi.sushi.mixin;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
+import java.util.Map;
+
+public class MixinLoaderForge implements IFMLLoadingPlugin {
+
+    public MixinLoaderForge() {
+        MixinBootstrap.init();
+        Mixins.addConfigurations("mixins.sushi.json", "mixins.baritone.json");
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
