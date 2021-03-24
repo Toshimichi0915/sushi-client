ee51eff0cfb4eca5a688e6ed7f8f0f8c56bcf552
Toshimichi0915 <main@toshimichi.net>
2021-03-25 04:25:49 +0900

    Do not show invalid config in ClickGUI

--- src/main/java/net/toshimichi/sushi/gui/theme/simple/SimpleModuleConfigComponent.java
+++ src/main/java/net/toshimichi/sushi/gui/theme/simple/SimpleModuleConfigComponent.java
@@ -13,6 +13,7 @@ public class SimpleModuleConfigComponent extends PanelComponent<ConfigComponent<
     public SimpleModuleConfigComponent(Theme theme, Module module) {
         setLayout(new FlowLayout(this, FlowDirection.DOWN));
         for (Configuration<?> conf : module.getConfigurations().getAll()) {
+            if (!conf.isValid()) continue;
             ConfigComponent<?> component = theme.newConfigComponent(conf);
             if (component == null) continue;
             add(component);
