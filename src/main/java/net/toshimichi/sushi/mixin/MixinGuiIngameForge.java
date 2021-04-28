8d93f51d42bf18f500c789a29f31ebf086e4c429
Toshimichi0915 <main@toshimichi.net>
2021-04-28 16:12:46 +0900

    Supress Mixin warning

--- src/main/java/net/toshimichi/sushi/mixin/MixinGuiIngameForge.java
+++ src/main/java/net/toshimichi/sushi/mixin/MixinGuiIngameForge.java
@@ -12,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiIngameForge.class)
 public class MixinGuiIngameForge {
 
-    @Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraftforge/client/GuiIngameForge;pre(Lnet/minecraftforge/client/event/RenderGameOverlayEvent$ElementType;)Z"), method = "renderGameOverlay")
+    @Inject(at = @At("HEAD"), method = "renderGameOverlay")
     public void onRenderGameOverlayHead(float partialTicks, CallbackInfo info) {
         EventHandlers.callEvent(new OverlayRenderEvent(EventTiming.PRE));
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;post(Lnet/minecraftforge/client/event/RenderGameOverlayEvent$ElementType;)V"), method = "renderGameOverlay")
+    @Inject(at = @At("TAIL"), method = "renderGameOverlay")
     public void onRenderGameOverlayTail(float partialTicks, CallbackInfo info) {
         EventHandlers.callEvent(new OverlayRenderEvent(EventTiming.POST));
     }
