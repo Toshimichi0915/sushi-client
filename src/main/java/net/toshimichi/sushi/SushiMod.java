67ae822a9cdb1f83e696da84ab9ec29a9ea73e66
Toshimichi0915 <main@toshimichi.net>
2021-04-29 22:38:56 +0900

    Change naming policy

--- src/main/java/net/toshimichi/sushi/SushiMod.java
+++ src/main/java/net/toshimichi/sushi/SushiMod.java
@@ -1,5 +1,6 @@
 package net.toshimichi.sushi;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -37,7 +38,7 @@ public class SushiMod {
     private static final Gson gson;
 
     static {
-        gson = new GsonBuilder().setPrettyPrinting().create();
+        gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
     }
 
     private final File baseDir = new File("./sushi");
