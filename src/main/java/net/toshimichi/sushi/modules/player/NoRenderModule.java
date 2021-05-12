ae0b1d47b98edd34e2408386e992dc7a046db4ae
Toshimichi0915 <main@toshimichi.net>
2021-05-12 23:09:15 +0900

    Add NoRender -> Map Icons

--- src/main/java/net/toshimichi/sushi/modules/player/NoRenderModule.java
+++ src/main/java/net/toshimichi/sushi/modules/player/NoRenderModule.java
@@ -1,5 +1,8 @@
 package net.toshimichi.sushi.modules.player;
 
+import io.netty.buffer.Unpooled;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.server.SPacketMaps;
 import net.minecraft.world.EnumSkyBlock;
 import net.toshimichi.sushi.config.Configuration;
 import net.toshimichi.sushi.config.Configurations;
@@ -7,17 +10,22 @@ import net.toshimichi.sushi.events.EventHandler;
 import net.toshimichi.sushi.events.EventHandlers;
 import net.toshimichi.sushi.events.EventTiming;
 import net.toshimichi.sushi.events.client.UpdateLightEvent;
+import net.toshimichi.sushi.events.packet.PacketReceiveEvent;
 import net.toshimichi.sushi.modules.*;
+
+import java.io.IOException;
 
 public class NoRenderModule extends BaseModule {
 
     private final Configuration<Boolean> skyLight;
     private final Configuration<Boolean> blockLight;
+    private final Configuration<Boolean> mapIcons;
 
     public NoRenderModule(String id, Modules modules, Categories categories, Configurations provider, ModuleFactory factory) {
         super(id, modules, categories, provider, factory);
         skyLight = provider.get("sky_light", "Sky Light", null, Boolean.class, false);
         blockLight = provider.get("block_light", "Block Light", null, Boolean.class, false);
+        mapIcons = provider.get("map_icons", "Map Icons", null, Boolean.class, false);
     }
 
     @Override
@@ -45,5 +53,32 @@ public class NoRenderModule extends BaseModule {
         if ((skyLight.getValue() && e.getEnumSkyBlock() == EnumSkyBlock.SKY) ||
                 (blockLight.getValue() && e.getEnumSkyBlock() == EnumSkyBlock.BLOCK))
             e.setCancelled(true);
+    }
+
+    @EventHandler(timing = EventTiming.PRE)
+    public void onPacketReceive(PacketReceiveEvent e) throws IOException {
+        if (!mapIcons.getValue()) return;
+        if (!(e.getPacket() instanceof SPacketMaps)) return;
+        SPacketMaps packet = (SPacketMaps) e.getPacket();
+        PacketBuffer read = new PacketBuffer(Unpooled.buffer());
+        PacketBuffer write = new PacketBuffer(Unpooled.buffer());
+        packet.writePacketData(read);
+        // map-id, mapScale, trackingPosition, iconsLength, icons, columns
+        write.writeVarInt(read.readVarInt());
+        write.writeByte(read.readByte());
+        write.writeBoolean(read.readBoolean());
+        int iconsLength = read.readVarInt();
+        write.writeVarInt(0);
+        read.skipBytes(iconsLength * 3);
+        byte columns = read.readByte();
+        write.writeByte(columns);
+        if (columns > 0) {
+            // rows, minX, minZ, mapDataBytes
+            write.writeByte(read.readByte());
+            write.writeByte(read.readByte());
+            write.writeByte(read.readByte());
+            write.writeByteArray(read.readByteArray());
+        }
+        packet.readPacketData(write);
     }
 }
