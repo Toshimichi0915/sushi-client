a0bd04d27f6ef35c8d1f6170c37d5b3936e90a4c
Toshimichi0915 <main@toshimichi.net>
2021-05-15 04:19:26 +0900

    Fix ClickGUI showing invalid configurations

--- src/main/java/net/toshimichi/sushi/gui/theme/simple/SimpleModuleCategoryComponent.java
+++ src/main/java/net/toshimichi/sushi/gui/theme/simple/SimpleModuleCategoryComponent.java
@@ -10,16 +10,19 @@ import net.toshimichi.sushi.gui.layout.FlowLayout;
 import net.toshimichi.sushi.gui.theme.Theme;
 import net.toshimichi.sushi.modules.Module;
 
+import java.util.ArrayList;
+
 public class SimpleModuleCategoryComponent extends BasePanelComponent<Component> {
 
+    private final Theme theme;
+    private final Module module;
+    private final ConfigurationCategory category;
+
     public SimpleModuleCategoryComponent(Theme theme, Module module, ConfigurationCategory category) {
+        this.theme = theme;
+        this.module = module;
+        this.category = category;
         setLayout(new FlowLayout(this, FlowDirection.DOWN));
-        for (Configuration<?> conf : module.getConfigurations().getByCategory(category)) {
-            if (!conf.isValid()) continue;
-            ConfigComponent<?> component = theme.newConfigComponent(conf);
-            if (component == null) continue;
-            add(component, true);
-        }
 
         sort((a, b) -> {
             int aPriority = 1000;
@@ -30,5 +33,31 @@ public class SimpleModuleCategoryComponent extends BasePanelComponent<Component>
                 bPriority = ((ConfigComponent<?>) b).getValue().getPriority();
             return Integer.compare(aPriority, bPriority);
         });
+    }
+
+    private boolean contains(Configuration<?> conf) {
+        for (Component component : this) {
+            if (!(component instanceof ConfigComponent)) continue;
+            if (((ConfigComponent<?>) component).getValue().equals(conf)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRelocate() {
+        for (Configuration<?> conf : module.getConfigurations().getByCategory(category)) {
+            if (!conf.isValid()) continue;
+            if (contains(conf)) continue;
+            ConfigComponent<?> component = theme.newConfigComponent(conf);
+            if (component == null) continue;
+            add(component, true);
+        }
+        for (Component component : new ArrayList<>(this)) {
+            if (!(component instanceof ConfigComponent)) continue;
+            if (((ConfigComponent<?>) component).getValue().isValid()) continue;
+            remove(component);
+        }
+
+        super.onRelocate();
     }
 }
