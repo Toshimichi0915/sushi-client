82664b9ba7546f88823f2bc34b7797b24edde829
Toshimichi0915 <main@toshimichi.net>
2021-05-19 03:48:52 +0900

    Fix random ConcurrentModificationException

--- src/main/java/net/toshimichi/sushi/mixin/MixinNetworkManager.java
+++ src/main/java/net/toshimichi/sushi/mixin/MixinNetworkManager.java
@@ -1,12 +1,12 @@
 package net.toshimichi.sushi.mixin;
 
-import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.toshimichi.sushi.events.EventHandlers;
-import net.toshimichi.sushi.events.packet.PacketReceiveEvent;
 import net.toshimichi.sushi.events.packet.PacketSendEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -15,8 +15,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(NetworkManager.class)
 public abstract class MixinNetworkManager {
 
+    @Shadow
+    public abstract void sendPacket(Packet<?> packetIn);
+
     @ModifyVariable(at = @At(value = "HEAD", ordinal = 0), method = "sendPacket(Lnet/minecraft/network/Packet;)V")
     public Packet<?> onModifySendPacket(Packet<?> packet) {
+        if (!Minecraft.getMinecraft().isCallingFromMinecraftThread()) return packet;
         PacketSendEvent event = new PacketSendEvent(packet);
         EventHandlers.callEvent(event);
         if (event.isCancelled())
@@ -27,25 +31,14 @@ public abstract class MixinNetworkManager {
 
     @Inject(at = @At("HEAD"), method = "sendPacket(Lnet/minecraft/network/Packet;)V", cancellable = true)
     public void onSendPacket(Packet<?> packet, CallbackInfo info) {
+        if (!Minecraft.getMinecraft().isCallingFromMinecraftThread()) {
+            Minecraft.getMinecraft().addScheduledTask(() -> sendPacket(packet));
+            info.cancel();
+            return;
+        }
         if (packet == null) {
             info.cancel();
         }
     }
 
-    @ModifyVariable(at = @At(value = "HEAD", ordinal = 0), method = "channelRead0")
-    public Packet<?> onModifyReceivePacket(Packet<?> packet) {
-        PacketReceiveEvent event = new PacketReceiveEvent(packet);
-        EventHandlers.callEvent(event);
-        if (event.isCancelled())
-            return null;
-        else
-            return event.getPacket();
-    }
-
-    @Inject(at = @At("HEAD"), method = "channelRead0", cancellable = true)
-    public void onReceivePacket(ChannelHandlerContext context, Packet<?> packet, CallbackInfo info) {
-        if (packet == null) {
-            info.cancel();
-        }
-    }
 }
