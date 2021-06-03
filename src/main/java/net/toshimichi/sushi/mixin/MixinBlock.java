a3805e0f7319ba9186bfc209e9d4a647708a74d4
Toshimichi0915 <main@toshimichi.net>
2021-06-03 18:33:03 +0900

    Fix all blocks being always bright

--- src/main/java/net/toshimichi/sushi/mixin/MixinBlock.java
+++ src/main/java/net/toshimichi/sushi/mixin/MixinBlock.java
@@ -2,8 +2,8 @@ package net.toshimichi.sushi.mixin;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
-import net.toshimichi.sushi.utils.BlockVisibility;
-import net.toshimichi.sushi.utils.XrayUtils;
+import net.toshimichi.sushi.utils.world.BlockVisibility;
+import net.toshimichi.sushi.utils.world.XrayUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,6 +14,6 @@ public class MixinBlock {
 
     @Inject(at = @At("HEAD"), method = "getLightValue(Lnet/minecraft/block/state/IBlockState;)I", cancellable = true)
     public void getLightValue(IBlockState state, CallbackInfoReturnable<Integer> cir) {
-        if (XrayUtils.getBlockVisibility(state.getBlock()) == BlockVisibility.VISIBLE) cir.setReturnValue(15);
+        if (XrayUtils.getBlockVisibility(state.getBlock()) == BlockVisibility.VISIBLE && XrayUtils.isEnabled()) cir.setReturnValue(15);
     }
 }
