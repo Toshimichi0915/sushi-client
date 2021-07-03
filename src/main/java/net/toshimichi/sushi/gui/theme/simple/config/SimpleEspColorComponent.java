d1f79121d398b3e3c37780f8b3afe46993d22bd3
Toshimichi0915 <main@toshimichi.net>
2021-07-03 21:18:24 +0900

    Remove Color Picker -> Alpha enabled

--- src/main/java/net/toshimichi/sushi/gui/theme/simple/config/SimpleEspColorComponent.java
+++ src/main/java/net/toshimichi/sushi/gui/theme/simple/config/SimpleEspColorComponent.java
@@ -24,9 +24,12 @@ public class SimpleEspColorComponent extends BasePanelComponent<Component> imple
     private final Configuration<Boolean> alphaEnabled;
     private final Configuration<IntRange> alpha;
     private final SimpleColorComponent colorComponent;
-    private final SimpleBooleanComponent rainbowComponent;
     private final SimpleBooleanComponent alphaEnabledComponent;
     private final SimpleIntRangeComponent alphaComponent;
+
+    private Color combine(Color c, int alpha) {
+        return new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha);
+    }
 
     public SimpleEspColorComponent(ThemeConstants constants, Configuration<EspColor> c) {
         this.constants = constants;
@@ -38,12 +41,12 @@ public class SimpleEspColorComponent extends BasePanelComponent<Component> imple
         this.alpha = new FakeConfiguration<>("alpha", "Alpha", null, IntRange.class, new IntRange(espColor.getColor().getAlpha(), 255, 0, 1));
 
         this.colorComponent = new SimpleColorComponent(constants, color);
-        this.rainbowComponent = new SimpleBooleanComponent(constants, rainbow);
         this.alphaEnabledComponent = new SimpleBooleanComponent(constants, alphaEnabled);
         this.alphaComponent = new SimpleIntRangeComponent(constants, alpha);
 
         c.addHandler(esp -> {
-            if (esp.getColor().getRGB() != color.getValue().getRGB()) color.setValue(esp.getColor());
+            if (esp.getColor().getRGB() != color.getValue().getRGB())
+                color.setValue(combine(esp.getColor(), alpha.getValue().getCurrent()));
             if (esp.isRainbow() != rainbow.getValue()) rainbow.setValue(esp.isRainbow());
             if (esp.isAlphaEnabled() != alphaEnabled.getValue()) alphaEnabled.setValue(esp.isAlphaEnabled());
             if (esp.getColor().getAlpha() != color.getValue().getAlpha())
@@ -59,11 +62,9 @@ public class SimpleEspColorComponent extends BasePanelComponent<Component> imple
         double marginLeft = colorComponent.getMarginLeft();
         double marginRight = colorComponent.getMarginRight();
         colorComponent.setMargin(new Insets(0, 0, 0, 0));
-        rainbowComponent.setMargin(new Insets(0, marginLeft, 0, marginRight));
         alphaEnabledComponent.setMargin(new Insets(0, marginLeft, 0, marginRight));
         alphaComponent.setMargin(new Insets(0, marginLeft, 2, marginRight));
         add(colorComponent);
-        add(rainbowComponent);
         add(alphaEnabledComponent);
         add(alphaComponent);
     }
