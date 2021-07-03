912a9db29875da6d6473bcf63b1fa8603bdad190
Toshimichi0915 <main@toshimichi.net>
2021-07-04 06:14:38 +0900

    Make AntiCevBreak based on damage

--- src/main/java/net/toshimichi/sushi/modules/combat/AntiCevBreakModule.java
+++ src/main/java/net/toshimichi/sushi/modules/combat/AntiCevBreakModule.java
@@ -1,10 +1,13 @@
 package net.toshimichi.sushi.modules.combat;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityEnderCrystal;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.network.play.client.CPacketUseEntity;
+import net.minecraft.util.math.BlockPos;
 import net.toshimichi.sushi.config.RootConfigurations;
 import net.toshimichi.sushi.events.EventHandler;
 import net.toshimichi.sushi.events.EventHandlers;
@@ -14,6 +17,7 @@ import net.toshimichi.sushi.modules.*;
 import net.toshimichi.sushi.task.forge.TaskExecutor;
 import net.toshimichi.sushi.task.tasks.BlockPlaceTask;
 import net.toshimichi.sushi.task.tasks.ItemSwitchTask;
+import net.toshimichi.sushi.utils.combat.DamageUtils;
 import net.toshimichi.sushi.utils.player.DesyncMode;
 import net.toshimichi.sushi.utils.player.PositionUtils;
 import net.toshimichi.sushi.utils.world.BlockPlaceInfo;
@@ -39,22 +43,27 @@ public class AntiCevBreakModule extends BaseModule {
 
     @EventHandler(timing = EventTiming.PRE)
     public void onClientTick(ClientTickEvent e) {
-        Item obsidian = Item.getItemById(49);
         for (Entity entity : getWorld().loadedEntityList) {
             if (!(entity instanceof EntityEnderCrystal)) continue;
             if (entity.posY < getPlayer().posY + 2) continue;
             double posX = getPlayer().posX, posY = getPlayer().posY + getPlayer().height, posZ = getPlayer().posZ;
             if (entity.getDistanceSq(posX, posY, posZ) > 4) continue;
-            Block floor = getWorld().getBlockState(entity.getPosition().add(0, -1, 0)).getBlock();
-            if (floor != Block.getBlockFromItem(obsidian)) return;
+            BlockPos floorPos = BlockUtils.toBlockPos(entity.getPositionVector()).add(0, -1, 0);
+            IBlockState floorState = getWorld().getBlockState(floorPos);
+            Block floor = floorState.getBlock();
+            if (floor != Blocks.OBSIDIAN) return;
+            getWorld().setBlockState(floorPos, Blocks.AIR.getDefaultState());
+            double damage = DamageUtils.getCrystalDamage(getPlayer(), entity.getPositionVector());
+            getWorld().setBlockState(floorPos, floorState);
+            if (damage <= 30) return;
             PositionUtils.desync(DesyncMode.POSITION);
             PositionUtils.move(posX, getPlayer().posY + 0.2, posZ, 0, 0, true, false, DesyncMode.POSITION);
             PositionUtils.pop();
             getConnection().sendPacket(new CPacketUseEntity(entity));
-            BlockPlaceInfo face = BlockUtils.findBlockPlaceInfo(getWorld(), entity.getPosition());
+            BlockPlaceInfo face = BlockUtils.findBlockPlaceInfo(getWorld(), BlockUtils.toBlockPos(entity.getPositionVector()));
             if (face == null) continue;
             TaskExecutor.newTaskChain()
-                    .supply(() -> obsidian)
+                    .supply(() -> Item.getItemFromBlock(Blocks.OBSIDIAN))
                     .then(new ItemSwitchTask(null, false))
                     .abortIfFalse()
                     .supply(() -> Collections.singletonList(face))
