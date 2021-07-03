147bdb8af22155e5840242b07767518473e8bc43
Toshimichi0915 <main@toshimichi.net>
2021-07-04 05:02:21 +0900

    Add background settings for ModuleListComponent

--- src/main/java/net/toshimichi/sushi/gui/hud/elements/ModuleListComponent.java
+++ src/main/java/net/toshimichi/sushi/gui/hud/elements/ModuleListComponent.java
@@ -1,18 +1,35 @@
 package net.toshimichi.sushi.gui.hud.elements;
 
 import net.toshimichi.sushi.Sushi;
+import net.toshimichi.sushi.config.Configuration;
 import net.toshimichi.sushi.config.Configurations;
+import net.toshimichi.sushi.config.data.EspColor;
+import net.toshimichi.sushi.config.data.IntRange;
 import net.toshimichi.sushi.gui.hud.BaseHudElementComponent;
 import net.toshimichi.sushi.modules.Module;
 import net.toshimichi.sushi.utils.render.GuiUtils;
 import net.toshimichi.sushi.utils.render.TextPreview;
 
+import java.awt.Color;
 import java.util.*;
 
 public class ModuleListComponent extends BaseHudElementComponent {
 
+    private final Configuration<EspColor> backgroundColor;
+    private final Configuration<IntRange> margin;
+    private final Configuration<IntRange> paddingTop;
+    private final Configuration<IntRange> paddingBottom;
+    private final Configuration<IntRange> paddingLeft;
+    private final Configuration<IntRange> paddingRight;
+
     public ModuleListComponent(Configurations configurations, String id, String name) {
         super(configurations, id, name);
+        backgroundColor = configurations.get(id + ".background_color", "Background Color", null, EspColor.class, new EspColor(Color.GRAY, true));
+        margin = configurations.get(id + ".margin", "Margin", null, IntRange.class, new IntRange(1, 10, 0, 1));
+        paddingTop = configurations.get(id + ".padding_top", "Padding Top", null, IntRange.class, new IntRange(1, 10, 0, 1));
+        paddingBottom = configurations.get(id + ".padding_bottom", "Padding Bottom", null, IntRange.class, new IntRange(1, 10, 0, 1));
+        paddingLeft = configurations.get(id + ".padding_left", "Padding Left", null, IntRange.class, new IntRange(1, 10, 0, 1));
+        paddingRight = configurations.get(id + ".padding_right", "Padding Right", null, IntRange.class, new IntRange(1, 10, 0, 1));
     }
 
     @Override
@@ -25,20 +42,34 @@ public class ModuleListComponent extends BaseHudElementComponent {
             TextPreview preview = GuiUtils.prepareText(module.getName(), getTextSettings("text").getValue());
             list.add(new AbstractMap.SimpleEntry<>(module, preview));
             if (totalWidth < preview.getWidth()) totalWidth = preview.getWidth();
-            totalHeight += preview.getHeight() + 1;
+            totalHeight += preview.getHeight() + margin.getValue().getCurrent();
         }
-        setWidth(totalWidth);
-        setHeight(totalHeight);
+        setWidth(totalWidth + paddingLeft.getValue().getCurrent() + paddingRight.getValue().getCurrent());
+        setHeight(totalHeight + paddingTop.getValue().getCurrent() + paddingBottom.getValue().getCurrent());
 
         list.sort(Comparator.comparingDouble(i -> i.getValue().getWidth()));
         if (!getOrigin().isFromBottom()) Collections.reverse(list);
 
         double height = 0;
+
+        // draw background
         for (Map.Entry<Module, TextPreview> entry : list) {
             TextPreview preview = entry.getValue();
             double width = getOrigin().isFromRight() ? getWidth() - preview.getWidth() : 0;
-            preview.draw(getWindowX() + width, getWindowY() + height);
-            height += preview.getHeight() + 1;
+            GuiUtils.drawRect(getWindowX() + width, getWindowY() + height,
+                    preview.getWidth() + paddingLeft.getValue().getCurrent() + paddingRight.getValue().getCurrent(),
+                    preview.getHeight() + margin.getValue().getCurrent() + paddingTop.getValue().getCurrent() + paddingBottom.getValue().getCurrent(),
+                    backgroundColor.getValue().getCurrentColor());
+            height += preview.getHeight() + margin.getValue().getCurrent();
+        }
+
+        height = 0;
+        // draw text
+        for (Map.Entry<Module, TextPreview> entry : list) {
+            TextPreview preview = entry.getValue();
+            double width = getOrigin().isFromRight() ? getWidth() - preview.getWidth() : 0;
+            preview.draw(getWindowX() + width + paddingLeft.getValue().getCurrent(), getWindowY() + height + paddingTop.getValue().getCurrent());
+            height += preview.getHeight() + margin.getValue().getCurrent();
         }
     }
 
