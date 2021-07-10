33e52bd23c329157391928f71e7c6ebf9363979e
Toshimichi0915 <main@toshimichi.net>
2021-07-10 15:28:03 +0900

    Move load resources to another class

--- src/main/java/net/toshimichi/sushi/mixin/MixinLoaderForge.java
+++ src/main/java/net/toshimichi/sushi/mixin/MixinLoaderForge.java
@@ -2,6 +2,7 @@ package net.toshimichi.sushi.mixin;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.toshimichi.sushi.hwid.gen.EncryptUtils;
+import net.toshimichi.sushi.utils.MixinGenerator;
 import org.apache.commons.io.IOUtils;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
@@ -20,32 +21,10 @@ import java.util.Map;
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 public class MixinLoaderForge implements IFMLLoadingPlugin {
 
-    private static final String CLASSPATH = "LICENSE";
     private static boolean isObfuscatedEnvironment = false;
 
-    private void loadResources() {
-        try {
-            File temp = Files.createTempFile(null, null).toFile();
-            temp.deleteOnExit();
-            byte[] bytes;
-            try (InputStream in = getClass().getClassLoader().getResourceAsStream(CLASSPATH)) {
-                if (in == null) return;
-                bytes = EncryptUtils.decrypt(EncryptUtils.getHWID(), IOUtils.toByteArray(in));
-            }
-            try (FileOutputStream out = new FileOutputStream(temp)) {
-                IOUtils.write(bytes, out);
-            }
-            Method addURL = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-            addURL.setAccessible(true);
-            addURL.invoke(getClass().getClassLoader(), temp.toURI().toURL());
-        } catch (Exception e) {
-            e.printStackTrace();
-            // skip
-        }
-    }
-
     public MixinLoaderForge() {
-        loadResources();
+        MixinGenerator.loadResources(this);
         MixinBootstrap.init();
         Mixins.addConfigurations("mixins.sushi.json");
         MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
