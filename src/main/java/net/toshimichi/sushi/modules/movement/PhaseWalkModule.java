1257896ee4ddaeca81f8bde4b6c482ac6455701c
Toshimichi0915 <main@toshimichi.net>
2021-07-17 03:37:47 +0900

    Improve PhaseWalk

--- src/main/java/net/toshimichi/sushi/modules/movement/PhaseWalkModule.java
+++ src/main/java/net/toshimichi/sushi/modules/movement/PhaseWalkModule.java
@@ -12,13 +12,15 @@ import net.toshimichi.sushi.config.data.IntRange;
 import net.toshimichi.sushi.events.EventHandler;
 import net.toshimichi.sushi.events.EventHandlers;
 import net.toshimichi.sushi.events.EventTiming;
+import net.toshimichi.sushi.events.player.PlayerAttackEvent;
 import net.toshimichi.sushi.events.player.PlayerPushEvent;
 import net.toshimichi.sushi.events.player.PlayerTravelEvent;
+import net.toshimichi.sushi.events.player.UserCheckEvent;
 import net.toshimichi.sushi.modules.*;
-import net.toshimichi.sushi.utils.player.DesyncCloseable;
 import net.toshimichi.sushi.utils.player.DesyncMode;
 import net.toshimichi.sushi.utils.player.MovementUtils;
 import net.toshimichi.sushi.utils.player.PositionUtils;
+import org.apache.commons.lang3.RandomUtils;
 
 import java.util.List;
 
@@ -27,29 +29,33 @@ public class PhaseWalkModule extends BaseModule {
     private final Configuration<DoubleRange> horizontal;
     private final Configuration<DoubleRange> delta;
     private final Configuration<IntRange> range;
-    private boolean clipping;
-    private DesyncCloseable closeable;
+    private final Configuration<Boolean> safe;
+    private PhaseWalkPlayer phaseWalkPlayer;
 
     public PhaseWalkModule(String id, Modules modules, Categories categories, RootConfigurations provider, ModuleFactory factory) {
         super(id, modules, categories, provider, factory);
         horizontal = provider.get("horizontal_speed", "Horizontal Speed", null, DoubleRange.class, new DoubleRange(1, 20, 0, 0.1, 1));
-        delta = provider.get("delta", "Delta", null, DoubleRange.class, new DoubleRange(0.5, 1, 0.1, 0.1, 1));
+        delta = provider.get("delta", "Delta", null, DoubleRange.class, new DoubleRange(0.5, 2, 0.1, 0.1, 1));
         range = provider.get("range", "Range", null, IntRange.class, new IntRange(3, 8, 0, 1));
+        safe = provider.get("safe", "Safe", null, Boolean.class, true);
     }
 
     @Override
     public void onEnable() {
         EventHandlers.register(this);
-        closeable = PositionUtils.desync(DesyncMode.POSITION);
+        phaseWalkPlayer = new PhaseWalkPlayer(getWorld());
+        int entityId = RandomUtils.nextInt(0, Integer.MAX_VALUE) + Integer.MIN_VALUE;
+        getWorld().addEntityToWorld(entityId, phaseWalkPlayer);
+        Minecraft.getMinecraft().setRenderViewEntity(phaseWalkPlayer);
     }
 
     @Override
     public void onDisable() {
         EventHandlers.unregister(this);
-        if (closeable != null) closeable.close();
-        if (clipping) {
-            PositionUtils.move(PositionUtils.getX(), PositionUtils.getY(), PositionUtils.getZ(), 0, 0, true, false, DesyncMode.NONE);
-        }
+        getWorld().removeEntityFromWorld(phaseWalkPlayer.getEntityId());
+        Minecraft.getMinecraft().setRenderViewEntity(Minecraft.getMinecraft().player);
+        phaseWalkPlayer = null;
+        getPlayer().noClip = false;
     }
 
     @EventHandler(timing = EventTiming.PRE)
@@ -71,26 +77,24 @@ public class PhaseWalkModule extends BaseModule {
 
     @EventHandler(timing = EventTiming.POST)
     public void onPostPlayerTravel(PlayerTravelEvent e) {
-        clipping = false;
         EntityPlayerSP player = Minecraft.getMinecraft().player;
         int step = range.getValue().getCurrent();
         boolean isAboveAir = false;
         for (int y = step; y >= -step - 1; y--) {
             AxisAlignedBB boundingBox = player.getEntityBoundingBox().offset(0, y, 0);
             boundingBox = boundingBox.grow(-(boundingBox.maxX - boundingBox.minX) / 2, 0, -(boundingBox.maxZ - boundingBox.minZ) / 2);
+            boundingBox = boundingBox.offset(0, delta.getValue().getCurrent() / 2, 0).grow(0, -delta.getValue().getCurrent() / 2, 0);
             List<AxisAlignedBB> collisions = player.world.getCollisionBoxes(null, boundingBox);
             if (collisions.isEmpty()) {
                 isAboveAir = true;
             } else if (isAboveAir) {
-                clipping = true;
                 double maxY = 0;
                 for (AxisAlignedBB collision : collisions) {
                     if (collision.maxY > maxY) {
                         maxY = collision.maxY;
                     }
                 }
-                PositionUtils.move(player.posX, maxY, player.posZ, 0, 0, true, false, DesyncMode.NONE);
-                PositionUtils.move(player.posX, maxY - delta.getValue().getCurrent(), player.posZ, 0, 0, true, false, DesyncMode.POSITION);
+                PositionUtils.move(player.posX, maxY - delta.getValue().getCurrent(), player.posZ, 0, 0, true, false, DesyncMode.NONE);
                 return;
             }
         }
@@ -98,6 +102,16 @@ public class PhaseWalkModule extends BaseModule {
         // safe walk
         PositionUtils.move(player.prevPosX, player.prevPosY, player.prevPosZ, 0, 0, true, false, DesyncMode.NONE);
         PositionUtils.move(player.prevPosX, PositionUtils.getY(), player.prevPosZ, 0, 0, true, false, DesyncMode.POSITION);
+    }
+
+    @EventHandler(timing = EventTiming.PRE)
+    public void onPlayerAttack(PlayerAttackEvent e) {
+        if (e.getTarget() == getPlayer()) e.setCancelled(true);
+    }
+
+    @EventHandler(timing = EventTiming.PRE)
+    public void onUserCheck(UserCheckEvent e) {
+        e.setUser(false);
     }
 
     @EventHandler(timing = EventTiming.PRE)
