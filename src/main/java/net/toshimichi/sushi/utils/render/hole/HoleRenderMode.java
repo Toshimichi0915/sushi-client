aaaca7478515cb20c5b14ea488fd7a3dcc3c3035
Toshimichi0915 <main@toshimichi.net>
2021-07-27 23:47:00 +0900

    Add HoleRenderMode -> OUTLINE/BOTTOM_OUTLINE

--- src/main/java/net/toshimichi/sushi/utils/render/hole/HoleRenderMode.java
+++ src/main/java/net/toshimichi/sushi/utils/render/hole/HoleRenderMode.java
@@ -31,6 +31,28 @@ public enum HoleRenderMode implements Named, HoleRenderer {
                     HoleRenderMode.getColor(info, obsidian, bedrock));
             glEnable(GL_DEPTH_TEST);
         }
+    },
+
+    @SerializedName("OUTLINE")
+    OUTLINE("Outline") {
+        @Override
+        public void render(World world, HoleInfo info, EspColor obsidian, EspColor bedrock) {
+            glDisable(GL_DEPTH_TEST);
+            RenderUtils.drawOutline(info.getBox(), HoleRenderMode.getColor(info, obsidian, bedrock), 3);
+            glEnable(GL_DEPTH_TEST);
+        }
+    },
+
+    @SerializedName("BOTTOM_OUTLINE")
+    BOTTOM_OUTLINE("Bottom Outline") {
+        @Override
+        public void render(World world, HoleInfo info, EspColor obsidian, EspColor bedrock) {
+            glDisable(GL_DEPTH_TEST);
+            double height = info.getBox().maxY - info.getBox().minY;
+            RenderUtils.drawOutline(info.getBox().grow(0, -height / 2, 0).offset(0, -height / 2, 0),
+                    HoleRenderMode.getColor(info, obsidian, bedrock), 3);
+            glEnable(GL_DEPTH_TEST);
+        }
     };
 
     private final String name;
