f4da40104191a1a69bc34476d7e64f29cc4718be
Toshimichi0915 <main@toshimichi.net>
2021-08-07 01:17:53 +0900

    Show items in Nametags

--- src/main/java/net/toshimichi/sushi/modules/render/NameTagsModule.java
+++ src/main/java/net/toshimichi/sushi/modules/render/NameTagsModule.java
@@ -1,8 +1,13 @@
 package net.toshimichi.sushi.modules.render;
 
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec2f;
 import net.toshimichi.sushi.config.Config;
@@ -21,10 +26,13 @@ import net.toshimichi.sushi.utils.EntityUtils;
 import net.toshimichi.sushi.utils.render.GuiUtils;
 import net.toshimichi.sushi.utils.render.RenderUtils;
 import net.toshimichi.sushi.utils.render.TextPreview;
-import org.lwjgl.opengl.GL11;
 
 import java.awt.Color;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Map;
+
+import static org.lwjgl.opengl.GL11.*;
 
 public class NameTagsModule extends BaseModule {
 
@@ -62,6 +70,9 @@ public class NameTagsModule extends BaseModule {
 
     @Config(id = "max_scale", name = "Max Scale")
     public IntRange maxScale = new IntRange(100, 100, 1, 1);
+
+    private static final double SCALE = 3;
+    private static final int MARGIN = 7;
 
     public NameTagsModule(String id, Modules modules, Categories categories, RootConfigurations provider, ModuleFactory factory) {
         super(id, modules, categories, provider, factory);
@@ -108,12 +119,15 @@ public class NameTagsModule extends BaseModule {
             Vec2f head = RenderUtils.fromWorld(RenderUtils.getInterpolatedPos(entity).add(0, entity.height, 0));
             if (head == null) continue;
 
+            glEnable(GL_DEPTH_TEST);
+            glEnable(GL_BLEND);
+
             // set up matrix
             double scale = RenderUtils.getScale(RenderUtils.getInterpolatedPos(entity)) * scaleMultiplier.getCurrent();
             scale = MathHelper.clamp(scale, minScale.getCurrent() / 100D, maxScale.getCurrent() / 100D);
-            GL11.glPushMatrix();
-            GL11.glTranslated(head.x, head.y, 0);
-            GL11.glScaled(scale, scale, 1);
+            glPushMatrix();
+            glTranslated(head.x, head.y, 0);
+            glScaled(scale, scale, 1);
 
             StringBuilder text = new StringBuilder(entity.getName());
             double health = entityLiving.getHealth() + entityLiving.getAbsorptionAmount();
@@ -124,13 +138,51 @@ public class NameTagsModule extends BaseModule {
             TextPreview preview = GuiUtils.prepareText(text.toString(), fontName, Color.WHITE, pts, false);
             double width = preview.getWidth();
             double height = preview.getHeight();
-            double x = -width / 2;
-            double y = -height - 5;
-            GuiUtils.drawRect(x - 5, y - 1, width + 10, height + 2, new Color(0, 0, 0, 100));
-            GuiUtils.drawOutline(x - 5, y - 1, width + 10, height + 2, new Color(0, 0, 0), 1);
-            preview.draw(x, y);
+            double textX = -width / 2;
+            double textY = -height - 5;
+            GuiUtils.drawRect(textX - 5, textY - 1, width + 10, height + 2, new Color(0, 0, 0, 100));
+            GuiUtils.drawOutline(textX - 5, textY - 1, width + 10, height + 2, new Color(0, 0, 0), 1);
+            preview.draw(textX, textY);
+            glPopMatrix();
+            glPushMatrix();
 
-            GL11.glPopMatrix();
+            glTranslated(head.x, head.y, 0);
+            glScaled(scale * SCALE, scale * SCALE, 0);
+            RenderItem renderer = getClient().getRenderItem();
+            double startX = -MARGIN * SCALE * 3;
+            ArrayList<ItemStack> inventory = new ArrayList<>();
+            inventory.add(entityLiving.getHeldItemMainhand());
+            if (entityLiving instanceof EntityPlayer) {
+                EntityPlayer other = (EntityPlayer) entityLiving;
+                inventory.add(other.inventory.armorInventory.get(3));
+                inventory.add(other.inventory.armorInventory.get(2));
+                inventory.add(other.inventory.armorInventory.get(1));
+                inventory.add(other.inventory.armorInventory.get(0));
+                inventory.add(other.inventory.offHandInventory.get(0));
+            }
+
+            int index = 0;
+            for (ItemStack item : inventory) {
+                int itemX = (int) (MARGIN * SCALE * index + startX);
+                int itemY = (int) (-SCALE * 12);
+                RenderHelper.enableGUIStandardItemLighting();
+                renderer.renderItemAndEffectIntoGUI(item, itemX, itemY);
+                renderer.renderItemOverlays(getClient().fontRenderer, item, itemX, itemY);
+                RenderHelper.disableStandardItemLighting();
+                glPushMatrix();
+                glTranslated(itemX, itemY, 0);
+                glScaled(0.3, 0.3, 0);
+                int index2 = 0;
+                for (Map.Entry<Enchantment, Integer> entry : EnchantmentHelper.getEnchantments(item).entrySet()) {
+                    String enchName = entry.getKey().getTranslatedName(entry.getValue()).substring(0, 2) + " " + entry.getValue();
+                    getClient().fontRenderer.drawString(enchName, 0, index2 * 9, Color.WHITE.getRGB());
+                    index2++;
+                }
+                glPopMatrix();
+                index++;
+            }
+
+            glPopMatrix();
         }
     }
 
