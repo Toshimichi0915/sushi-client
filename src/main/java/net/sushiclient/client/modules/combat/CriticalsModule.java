ac1367e14195d5f9a0f2e53436ebc693abb6a4a4
Toshimichi0915 <main@toshimichi.net>
2021-10-07 16:56:23 +0900

    Add Criticals -> Motion Check

--- src/main/java/net/sushiclient/client/modules/combat/CriticalsModule.java
+++ src/main/java/net/sushiclient/client/modules/combat/CriticalsModule.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketUseEntity;
+import net.sushiclient.client.config.Configuration;
 import net.sushiclient.client.config.RootConfigurations;
 import net.sushiclient.client.events.EventHandler;
 import net.sushiclient.client.events.EventHandlers;
@@ -12,8 +13,12 @@ import net.sushiclient.client.events.packet.PacketSendEvent;
 import net.sushiclient.client.modules.*;
 
 public class CriticalsModule extends BaseModule {
+
+    private final Configuration<Boolean> motionCheck;
+
     public CriticalsModule(String id, Modules modules, Categories categories, RootConfigurations provider, ModuleFactory factory) {
         super(id, modules, categories, provider, factory);
+        motionCheck = provider.get("motion_check", "Motion Check", null, Boolean.class, true);
     }
 
     @Override
@@ -33,6 +38,7 @@ public class CriticalsModule extends BaseModule {
         Entity entity = packet.getEntityFromWorld(getWorld());
         if (!(entity instanceof EntityLivingBase)) return;
         if (packet.getAction() != CPacketUseEntity.Action.ATTACK) return;
+        if (motionCheck.getValue() && getPlayer().motionY < 0) return;
         sendPacket(new CPacketPlayer.Position(getPlayer().posX, getPlayer().posY + 0.1, getPlayer().posZ, false));
         sendPacket(new CPacketPlayer.Position(getPlayer().posX, getPlayer().posY, getPlayer().posZ, false));
     }
